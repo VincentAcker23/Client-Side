b991ae4c09d3c8944bff80acba29214522909430
VincentAcker23 <97539306+VincentAcker23@users.noreply.github.com>
2022-03-22 22:52:19 -0500

    Add files via upload

---
+++ Module4/Module4Assignment/main.js
@@ -0,0 +1,28 @@
+let button = document.querySelector("#button");
+let restart = document.querySelector("#restart");
+let textfield = document.querySelector("#textfield");
+let msg = document.querySelector("#msg");
+button.addEventListener("click",onSumbit);
+restart.addEventListener("click",start);
+start();
+function start() {
+    random();
+}
+function onSumbit(e) {
+    e.preventDefault();
+    let val = textfield.value;
+    let num_to_compare = random();
+    if (val==num_to_compare.toString()){
+        msg.textContent = "Congratulations!! You guessed the correct ";
+    }
+    else {
+        msg.textContent = "Too Low";
+        console.log(val);
+        console.log(num_to_compare);
+    }
+}
+function random() {
+    let num = Math.random()*10;
+    let finalnum = Math.ceil(num);
+    return finalnum;
+}
