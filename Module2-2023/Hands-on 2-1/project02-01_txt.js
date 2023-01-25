81ccc8e70b5b5d882cc78f0ed755dbb5abbb9fa3
VincentAcker23 <97539306+VincentAcker23@users.noreply.github.com>
2023-01-25 12:16:20 -0600

    Add files via upload

--- Module2-2023/Hands-on 2-1/project02-01_txt.js
+++ Module2-2023/Hands-on 2-1/project02-01_txt.js
@@ -3,9 +3,18 @@
       Project 02-01
 
       Celsius <-> Farenheit Coverter
-      Author: 
-      Date:   
+      Author: Vincent Acker Jr      
+      Date:   1/25/23
 
       Filename: project02-01.js
  */
+          function convert() {
+            var degF = document.getElementByID("fValue").Value;
+            var degC = (degF - 32) * (5 / 9);
+            
+          document.getElementByID("cValue").innerHTML = degC;
+          }
+          document.getElementByID("cbutton").addEventListener("click", convert, false);
+
+
 
