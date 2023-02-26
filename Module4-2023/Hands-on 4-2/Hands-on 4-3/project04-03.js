5056362414d6b9f1764435950a570ae62bc12cee
VincentAcker23 <97539306+VincentAcker23@users.noreply.github.com>
2023-02-26 13:46:03 -0600

    Add files via upload

--- Module4-2023/Hands-on 4-2/Hands-on 4-3/project04-03.js
+++ Module4-2023/Hands-on 4-2/Hands-on 4-3/project04-03.js
@@ -1,44 +1,45 @@
-/*    JavaScript 7th Edition
-      Chapter 4
-      Project 04-03
+// TODO: 3: ASKING FOR STRICT INTERPRETATION
+'use strict';
 
-      Application to count the number of characters in a review comment
-      Author: 
-      Date:   
-
-      Filename: project04-03.js
-*/
-
+// TODO 6: ADDED ONE MORE FORWARD SLASH TO FIX THE COMMENT ERROR 
 // Maximum Length of Review
-const MAX_REVIEW = 100;
-document.getElementById("limit").innerHTML = MAX_REVIEW;
+const MAX_COUNT = 1000;
+document.getElementById("limit").innerHTML = MAX_COUNT;
 
 // Reference to elemets in the web page
-wordCountBox = document.getElementById("countValue");
-warningBox = document.getElementById("warningBox");
+// TODO 6: USING const KEYWORD TO DECLARE THE VARIABLES. EARLIER NO VARIABLE DECLARATION KEYWORD WAS USED
+const wordCountBox = document.getElementById("countValue");
+const warningBox = document.getElementById("warningBox");
 
 
 // Event listener for typing into the comment box
-document.getElementById("comment").addEventListener(keyup, updateCount);
+// TODO 6: keyup IS SUPPOSED TO BE A STRING
+document.getElementById("comment").addEventListener('keyup', updateCount);
 
 // Function to update the count with each keyup event
 function updateCount() {
-   // Set the warning box to an empty text string 
-   warningBox.innerHTML = "";
-   
-   // Count the number of characters in the comment box
-   commentText = document.getElementById("comment").value;
-   charCount = countCharacters(commentsText);
-   
+  // Set the warning box to an empty text string
+  warningBox.innerHTML = "";
+
+  // Count the number of characters in the comment box
+  // TODO 6: USING const KEYWORD TO DECLARE THE VARIABLES
+  const commentText = document.getElementById("comment").value;
+  const charCount = countCharacters(commentText); // TODO 6: FIX THE TYPE IN THE NAME
+
+  // TODO: 4: TRY CATCH
+  try {
+    // if the char count is greater than the value of the MAX_REVIEW constant then throw an exception
+    if (charCount > MAX_COUNT) {
+      throw 'You have exceeded the character count limit.'
+    }
+  } catch (e) {
+    // if an exception is caught then display the error message
+    warningBox.innerHTML = e
+  } finally {
+    // this will execute irrespective of if exception was thrown or not
+    wordCountBox.innerHTML = charCount
+  }
 }
-
-
-
-
-
-
-
-
 
 /*=================================================================*/
 // Function to count the number of characters in a text string
@@ -46,4 +47,4 @@ function countCharacters(textStr) {
    var commentregx = /\s/g;
    var chars = textStr.replace(commentregx, "");
    return chars.length;
-} 
+}
