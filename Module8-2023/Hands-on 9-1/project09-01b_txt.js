89515454fffb27a2a757a2324024ae0705eab949
Vincent Acker Jr <97539306+VincentAcker23@users.noreply.github.com>
2024-04-13 01:24:30 -0500

    Update project09-01b_txt.js

--- Module8-2023/Hands-on 9-1/project09-01b_txt.js
+++ Module8-2023/Hands-on 9-1/project09-01b_txt.js
@@ -9,4 +9,28 @@
 
       Filename: project09-01b.js
 */
+"use strict";
 
+/* JavaScript 7th Edition
+   Chapter 9
+   Project 09-01
+
+   Project to read field values from a query string
+   Author: [Your Name]
+   Date: [Current Date]
+   Filename: project09-01b.js
+*/
+
+let query = location.search.slice(1, location.search.length);
+query = decodeURIComponent(query);
+query = query.replaceAll("+", " ");
+
+let cardFields = query.split("&");
+let nameValue, name, value;
+
+for (let x in cardFields) {
+   nameValue = cardFields[x].split("=")
+   name = nameValue[0];
+   value = nameValue[1];
+   document.getElementById(name).textContent = value;
+}
