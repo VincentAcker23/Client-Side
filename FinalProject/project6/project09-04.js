12e1f9f386c708be0b8cd98674e2764db21adb82
Vincent Acker Jr <97539306+VincentAcker23@users.noreply.github.com>
2024-04-25 18:43:31 -0500

    Add files via upload

--- FinalProject/project6/project09-04.js
+++ FinalProject/project6/project09-04.js
@@ -1,48 +1,64 @@
 "use strict";
+/*    JavaScript 7th Edition
+      Chapter 9
+      Project 09-04
 
-function processCookie() {
+      Project to store high scores from a game in a cookie
+      Author: 
+      Date:   
+
+      Filename: project09-04.js
+*/
+
+/* Page Objects */
+let bestText = document.getElementById("best");
+let clockTimer = document.getElementById("timer");
+
+// Custom event that runs when the puzzle is solved
+window.addEventListener("puzzleSolved", updateRecord);
+
+// Event listener that is run when the page loads
+window.addEventListener("load", function() {
   
-  document.cookie ="username=" +
-  document.getElementById("usernameinput").value;
-}
-
-function populateInfo() {
-  if (document.cookie) {
-    var uname = document.cookie;
-    uname = uname.substring(uname.lastIndexOf("=") +1);
-    document.getElementById("usernameinput").value = uname;
-
-  }
-}
-
-function handleSubmit(evt) {
-  if (evt.preventDefault) {
-    evt.preventDefault();
-  }
-  else {
-    evt.returnValue = false;
-  }
-  processCookie();
-  document.getElementsByTagName("form")[0].submit();
-
-}
-function createEventListener(){
-  var loginForm = document.getElementsByTagName("form")[0];
-  if (loginForm.addEventListener) {
-    loginForm.addEventListener("submit", handleSubmit, false);
-
-  }
-  else if (loginForm.attachEvent) {
-      loginForm.attachEvent("onsubmit", handleSubmit);
+    // Check if the document.cookie object exists
+    if (document.cookie) {
+        // Retrieve the best time from cookies
+        let bestTime = getBestTime();
+        // Change the text content of the bestText object
+        document.getElementById("bestText").textContent = `${bestTime} seconds`;
     }
+});
+
+function getBestTime() {
+  // Check if the document.cookie object exists
+  if (document.cookie) {
+      // Split the document cookie text at the "=" character
+      let cookieArray = document.cookie.split('=');
+      // Convert the cookie value to an integer
+      return parseInt(cookieArray[1]);
+  } else {
+      // If document.cookie object doesn't exist, return a default value of 9999
+      return 9999;
   }
-  function setUpPage() {
-    populateInfo(); 
-    createEventListener();
+}
+function updateRecord() {
+  // Get the solution time and convert it to an integer
+  let solutionTime = parseInt(document.getElementById("timer").textContent);
+
+  // Get the current best time
+  let bestTime = getBestTime();
+
+  // If the current solution time is less than the best time, update the best time
+  if (solutionTime < bestTime) {
+      bestTime = solutionTime;
+
+      // Change the text content of the bestText object
+      document.getElementById("bestText").textContent = `${bestTime} seconds`;
+
+      // Write the best time to the document.cookie object with a max age of 90 days
+      let expirationDate = new Date();
+      expirationDate.setDate(expirationDate.getDate() + 90);
+      document.cookie = `best=${bestTime};expires=${expirationDate.toUTCString()}`;
   }
-  if (window.addEventListener) {
-    window.addEventListener("load", setUpPage, false);
-  }
-  else if (window.attachEvent) {
-    window.attachEvent("onload", setUpPage);
-  }
+}
+
